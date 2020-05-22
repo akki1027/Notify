75a1c1bf3d982f6c2bfaaf77b9a16c2cbc7c9642
akki1027 <akki10271995@gmail.com>
2020-05-22 14:00:34 +0000

    Create Tweet model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_131624) do
+ActiveRecord::Schema.define(version: 2020_05_22_134538) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
